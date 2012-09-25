21671864b7387334bc7f8bffffb8bea96a09d44c
Martin Malina <anyberry@gmail.com>
2012-09-25 10:31:27 +0200

    Added a script for the tests filter and added it to filter-repos.sh as well..
    filter_tests.py is a clone of filter_repo.py with a fixed regexp for integration tests migration and also a rename of the paths in the same iteration. So we end up with the structure of tests/org.plugin.something.bot.test.

---
+++ filter_tests.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import sys
+from git_fast_filter import Blob, Reset, FileChanges, Commit, FastExportFilter
+from git_fast_filter import get_commit_count, get_total_objects
+import re
+import datetime
+
+if len(sys.argv) != 4:
+  raise SystemExit("Syntax:\n  %s SOURCE_REPO TARGET_REPO REGULAR_EXP_FILTER")
+source_repo = sys.argv[1]
+target_repo = sys.argv[2]
+regexp = re.compile('^[^/]*/[^/]*/[^/]*\.bot.*')
+
+start = datetime.datetime.now()
+
+total_objects = get_total_objects(source_repo)  # blobs + trees
+total_commits = get_commit_count(source_repo)
+object_count = 0
+commit_count = 0
+
+def print_progress():
+  global object_count, commit_count, total_objects, total_commits
+  print "\rRewriting commits... %d/%d  (%d objects)" \
+        % (commit_count, total_commits, object_count),
+
+def my_blob_callback(blob):
+  global object_count
+  object_count += 1
+  print_progress()
+  
+def my_commit_callback(commit):
+  global commit_count
+  commit_count += 1
+  print_progress()
+  new_file_changes = []
+  for change in commit.file_changes:
+      if regexp.match(change.filename):
+        change.filename = re.sub('^[^/]*/[^/]*/org','tests/org',change.filename)
+        new_file_changes.append(change)
+        #print commit.branch + ":" + change.filename
+
+  commit.file_changes = new_file_changes
+  
+filter = FastExportFilter(blob_callback   = my_blob_callback,
+                          commit_callback = my_commit_callback)
+filter.run(source_repo, target_repo)
+
+end = datetime.datetime.now()
+
+print "End : " + str(end-start)
+
