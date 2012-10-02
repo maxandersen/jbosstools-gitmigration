b70106c2c23b5eef777f5eca68b255b113a2d77d
Max Rydahl Andersen <max@jboss.org>
2012-10-02 12:18:27 +0200

    include tags too

--- deleteemptybranches.sh
+++ deleteemptybranches.sh
@@ -1,3 +1,5 @@
+## this will treat $1 as a repository and go through it and delete all branches and tags with empty content.
+
 export GIT_DIR=$1/.git
 export GIT_WORK_TREE=$1
 
@@ -10,5 +12,16 @@ do
  if [[ "$NOFILES" == "0" ]]
   then
      git branch -D $REALBRANCH 
+  fi
+done
+
+git tag | while read BRANCH
+do
+ REALBRANCH=`echo "$BRANCH" | sed -e 's/\*//g'`
+ NOFILES=`git ls-tree $REALBRANCH | wc -l | tr -d ' '`
+# echo $NAME "$REALBRANCH" $NOFILES
+ if [[ "$NOFILES" == "0" ]]
+  then
+     git tag -d $REALBRANCH 
   fi
 done
